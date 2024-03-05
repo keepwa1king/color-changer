e0dfe1a311507ebf7a9f1d17667d83773fce6323
shinyo17 <17shinyo17@gmail.com>
2024-03-05 14:17:47 +0900

    Initial commit

---
+++ changeColor.js
@@ -0,0 +1,5 @@
+document.documentElement.style.setProperty('--bg-color-primary', '#0E0F37');
+document.documentElement.style.setProperty('--color-primary', '#0E0F37');
+
+document.documentElement.style.setProperty('--bg-color-secondary', '#C9AB13');
+document.documentElement.style.setProperty('--color-secondary', '#C9AB13');
